b3779c5259bbb16efb2a48838f08aaaa6208d975
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-09-10 12:23:00 +0430

    searchService init

--- BackEnd/ElasticLib/Handlers/SearchHandler.cs
+++ BackEnd/ElasticLib/Handlers/SearchHandler.cs
@@ -1,7 +1,17 @@
-﻿namespace ElasticLib.Handlers
+﻿using System.Collections.Generic;
+
+namespace ElasticLib.Handlers
 {
     public class SearchHandler
     {
-        
+        public IEnumerable<T> FindNodeInfo<T>()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<T> FindEdgeInfo<T>()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
