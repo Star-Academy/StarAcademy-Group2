ff603628a0d4bec97436268d1bf6899b844427cd
MJSahebnasi <mjsahebnasi@gmail.com>
2020-09-15 20:55:39 +0430

    Flow looks compelete - GraphLib netstandard2.1

--- BackEnd/GraphLogicLib/MaxFlowFinder.cs
+++ BackEnd/GraphLogicLib/MaxFlowFinder.cs
@@ -8,7 +8,7 @@ namespace GraphLogicLib
 {
     class MaxFlowFinder
     {
-        
+
         /*
             we assume there is no parallel(multiple) edge in the graph
         */
@@ -30,16 +30,12 @@ namespace GraphLogicLib
 
         public long Find()
         {
-            var output = 0;
+            long result = 0;
 
             while (bfs())
-            {
-
-
-
-            }
-
-            return output;
+                result += sendFlow(sourceId, new List<SimpleEdge>());
+            
+            return result;
         }
 
         private bool bfs()
@@ -49,15 +45,15 @@ namespace GraphLogicLib
 
             queue.Enqueue(sourceId);
             Levels[sourceId] = 0;
-            
+
             while (queue.Any())
             {
                 var node = queue.Dequeue();
                 int currnetLevel = Levels[node] + 1;
                 HashSet<SimpleEdge> neighbourEdges;
-                
+
                 //TODO to remove
-                if(!Graph.TryGetValue(node, out neighbourEdges))
+                if (!Graph.TryGetValue(node, out neighbourEdges))
                     throw new Exception("wtf ._.");
 
                 foreach (var neighbour in
@@ -78,30 +74,58 @@ namespace GraphLogicLib
 
         private long sendFlow(string node, List<SimpleEdge> path)
         {
-            if(node.Equals(destinationId))
+            if (node.Equals(destinationId))
             {
                 return updateCapacity(path);
             }
+            
+            long result = 0;
+
+            foreach(var edge in Graph[node])
+            {
+                if(edge.SourceAccount.Equals(node) && edge.RemainingCapacity > 0 
+                    && Levels[edge.DestinationAccount] == Levels[node]+1)
+                {
+                    path.Add(edge);
+                    result += sendFlow(edge.DestinationAccount, path);
+                    path.Remove(edge);
+                }
+            }
+
+            return result;
         }
 
         private long updateCapacity(List<SimpleEdge> path)
         {
             long flow = path.Min(edge => edge.RemainingCapacity);
-            
-            foreach(var edge in path)
+
+            foreach (var edge in path)
             {
                 edge.Flow += flow;
-                SimpleEdge reversed = new SimpleEdge()
+
+                SimpleEdge equalReversed = new SimpleEdge()
                 {
                     SourceAccount = edge.DestinationAccount,
-                    DestinationAccount = edge.SourceAccount    
+                    DestinationAccount = edge.SourceAccount
+                    //equals() is only based on these 2
                 };
-                if(Graph[edge.SourceAccount].Contains(reversed))
+
+                if (Graph[edge.DestinationAccount].TryGetValue(equalReversed, out var actualReversed))
                 {
-                    Graph[edge.SourceAccount].remove(reversed);
-                    
+                    actualReversed.Flow -= flow;
+                    // what happens to the one in "DestAcount" 's set?
+                }
+                else
+                {
+                    equalReversed.Capacity = edge.Capacity;
+                    //what happens to the other fields?
+
+                    Graph[edge.DestinationAccount].Add(equalReversed);
+                    Graph[edge.SourceAccount].Add(equalReversed);
                 }
             }
+
+            return flow;
         }
     }
 }
