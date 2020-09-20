1e2c1383d3f8ced0f5998fbe0b7b9f403cc387ad
Danyal Farahany <farahany.danyal@gmail.com>
2020-09-20 22:43:47 +0430

    expand query rectified

--- BackEnd/MyWebApi/Controllers/UserQueryController.cs
+++ BackEnd/MyWebApi/Controllers/UserQueryController.cs
@@ -25,15 +25,11 @@ namespace MyWebApi.Controllers
         [Route("searchNode")]
         public ActionResult<IEnumerable<Node>> NodeSearch([FromBody] NodeSearchQuery nodeSearchQuery)
         {
-            // Console.WriteLine(nodeSearchQuery);
-            // var nodeSearchQuery = JsonSerializer.Deserialize<NodeSearchQuery>(json);
             try
             {
                 var result = elasticService.Search<Node>(nodeSearchQuery);
                 return Ok(result);
             }
-            //TODO
-            //dummy code
             catch (Exception e)
             {
                 return BadRequest(e.Message);
@@ -44,71 +40,42 @@ namespace MyWebApi.Controllers
         [Route("expand")]
         public ActionResult<List<Tuple<HashSet<Node>, HashSet<Edge>>>> Expand([FromBody] ExpandQuery expandQuery)
         {
-            // var expandQuery = JsonSerializer.Deserialize<ExpandQuery>(json);
             var output = new List<Tuple<HashSet<Node>, HashSet<Edge>>>();
-
-            foreach (var acc in expandQuery.Accounts)
+            var nodes = new HashSet<Node>();
+            var edges = new HashSet<Edge>();
+            var accs = String.Join(' ',expandQuery.Accounts);
+            var incomingEdgeSearchQuery = new EdgeSearchQuery()
             {
-                var nodes = new HashSet<Node>();
-                var edges = new HashSet<Edge>();
-                var incomingEdgeSearchQuery = new EdgeSearchQuery()
-                {
-                    DestinationAccount = acc
-                };
-                var outcomingEdgeSearchQuery = new EdgeSearchQuery()
-                {
-                    SourceAccount = acc
-                };
-                incomingEdgeSearchQuery.SetFiltersFrom(expandQuery);
-                outcomingEdgeSearchQuery.SetFiltersFrom(expandQuery);
-                foreach (var edge in elasticService.Search<Edge>(incomingEdgeSearchQuery))
-                {
-                    var ns = new NodeSearchQuery()
-                    {
-                        AccountId = edge.SourceAccount
-                    };
-                    nodes.UnionWith(elasticService.Search<Node>(ns));
-                    edges.Add(edge);
-                }
-                foreach (var edge in elasticService.Search<Edge>(outcomingEdgeSearchQuery))
-                {
-                    var ns = new NodeSearchQuery()
-                    {
-                        AccountId = edge.DestinationAccount
-                    };
-                    nodes.UnionWith(elasticService.Search<Node>(ns));
-                    edges.Add(edge);
-                }
-                output.Add(new Tuple<HashSet<Node>, HashSet<Edge>>(nodes, edges));
+                DestinationAccount = accs
+            };
+            var outcomingEdgeSearchQuery = new EdgeSearchQuery()
+            {
+                SourceAccount = accs
+            };
+            incomingEdgeSearchQuery.SetFiltersFrom(expandQuery);
+            outcomingEdgeSearchQuery.SetFiltersFrom(expandQuery);
+            var nodesId = new HashSet<string>();
+            foreach (var edge in elasticService.Search<Edge>(incomingEdgeSearchQuery))
+            {
+                nodesId.Add(edge.SourceAccount);
+                edges.Add(edge);
             }
+            foreach (var edge in elasticService.Search<Edge>(outcomingEdgeSearchQuery))
+            {
+                nodesId.Add(edge.DestinationAccount);
+                edges.Add(edge);
+            }
+            nodes.UnionWith(elasticService
+                .Search<Node>(
+                    new NodeSearchQuery()
+                    {
+                        AccountId = String.Join(' ', nodesId)
+                    }
+                )
+            );
+            output.Add(new Tuple<HashSet<Node>, HashSet<Edge>>(nodes, edges));
             return output;
         }
-
-
-        // To improve expand
-
-
-
-        // [HttpPost]
-        // [Route("testnewexpand")]
-        // public ActionResult<List<Tuple<HashSet<Node>, HashSet<Edge>>>> Sth(/*[FromBody] string json*/)
-        // {
-        //     // var expandQuery = JsonSerializer.Deserialize<ExpandQuery>(json);
-        //     var output = new List<Tuple<HashSet<Node>, HashSet<Edge>>>();
-        //     var a = String.Join(' ',new List<string>(){
-        //         "6039548046",
-        //         "9862369812"
-        //     });
-        //     var outcomingEdgeSearchQuery = new EdgeSearchQuery()
-        //         {
-        //             SourceAccount = a
-        //         };
-        //     var b = elasticService.Search<Edge>(outcomingEdgeSearchQuery);
-        //     var g = from p in b
-        //             group p by p.SourceAccount into something
-        //             select new {Source = something.Key , Edges = something.ToList() };
-        //     return output;
-        // }
 
         [HttpPost]
         [Route("flow")]
