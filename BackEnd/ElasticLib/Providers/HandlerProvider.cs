0a4dccd5c8f3cf07655122654882da134e019f7f
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-09-21 20:30:52 +0330

    delete user [endpoint + elastic]

--- BackEnd/ElasticLib/Providers/HandlerProvider.cs
+++ BackEnd/ElasticLib/Providers/HandlerProvider.cs
@@ -7,12 +7,14 @@ namespace ElasticLib.Providers
         public ImportHandler ImportHandler { get; }
         public SearchHandler SearchHandler { get; }
         public AutoCompleteHandler AutoCompleteHandler { get; }
+        public DeleteHandler DeleteHandler { get; }
 
         public HandlerProvider()
         {
             ImportHandler = new ImportHandler();
             SearchHandler = new SearchHandler();
             AutoCompleteHandler = new AutoCompleteHandler();
+            DeleteHandler = new DeleteHandler();
         }
     }
 }
